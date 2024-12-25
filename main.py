44604b1222f095467c2bb5c51989ff62f1cab781
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-25 18:41:53 +0100

    More features, popups, etc.
    It took a little longer to do since I didnt know I have to use AsyncImage instead of normal Image, it is now fixed.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.uix.image import AsyncImage
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
@@ -5,48 +6,76 @@ import requests
 
 APIKey = ""
 url = f"https://api.spoonacular.com/recipes/findByIngredients"
+NRecipes = 20
+IgnP = True
 
 def searchByIngredientsAPI(ingredients):
-    print(ingredients)
+    #print(ingredients)
     paramss = {
         "ingredients": ingredients,
-        "number": 20,
+        "number": NRecipes,
         "ranking": 1,
-        "ignorePantry": True,
+        "ignorePantry": IgnP,
         "apiKey": APIKey
     }
     data = requests.get(url, params=paramss)
     recipes = data.json()
     if data.status_code == 200:
-        for recipe in recipes:
-            print(recipe["title"])
+        #for recipe in recipes:
+            #print(recipe["title"])
+            #print(recipe["usedIngredientCount"])
         return recipes
     else:
         print("Error")
         return None
     
 class RecipePopup(Popup):
-    pass
+    def __init__(self, recipe):
+        super(RecipePopup, self).__init__()
+        self.recipe = recipe
+        print(recipe["image"])
+        self.ids.ImageR.add_widget(AsyncImage(source=recipe["image"]))
 
 class RecipeWidget(GridLayout):
     def __init__(self, recipe):
         super(RecipeWidget, self).__init__()
         self.recipe = recipe
-        self.ids.recipeName.text = recipe["title"]
+        title = recipe["title"]
+        if len(title) > 20:
+            title = title[:20] + "..."
+        self.ids.recipeName.text = title
+        self.ids.ingredients.text = str(recipe["usedIngredientCount"]) + '/' + str(recipe["missedIngredientCount"] + recipe["usedIngredientCount"])
     def view(self):
         popup = RecipePopup(self.recipe)
         popup.open()
 
 class SettingsPopup(Popup):
     def __init__(self, caller):
+        global APIKey, NRecipes, IgnP
         super(SettingsPopup, self).__init__()
         self.caller = caller
+        self.ids.apiKeyInput.text = APIKey
+        self.ids.numRecipesInput.text = str(NRecipes)
+        self.ids.ignorePantry.active = IgnP
+    def save(self):
+        global APIKey, NRecipes, IgnP
+        if self.ids.apiKeyInput.text != "":
+            APIKey = self.ids.apiKeyInput.text
+        if self.ids.numRecipesInput.text != "":
+            NRecipes = int(self.ids.numRecipesInput.text)
+        IgnP = self.ids.ignorePantry.active
+        self.dismiss()
+
+class InfoWidget(GridLayout):
+    pass
 
 class MainGrid(GridLayout):
     def search(self):
         self.recipes = searchByIngredientsAPI(self.ids.searchInput.text)
         if self.recipes != None:
             self.ids.mealList.clear_widgets()
+            self.ids.mealList.height = 60
+            self.ids.mealList.add_widget(InfoWidget())
             for recipe in self.recipes:
                 self.ids.mealList.height += 50
                 self.ids.mealList.add_widget(RecipeWidget(recipe))
